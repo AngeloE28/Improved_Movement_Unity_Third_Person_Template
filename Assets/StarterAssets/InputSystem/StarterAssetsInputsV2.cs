4298807cb7d4090b2b87bfcf5b47fe2ffee93590
AngeloE28 <66254064+AngeloE28@users.noreply.github.com>
2022-08-31 05:14:47 +1000

    Initial commit

---
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputsV2.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputsV2 : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+		public void OnMove(InputAction.CallbackContext context)
+		{
+			MoveInput(context.ReadValue<Vector2>());
+		}
+
+		public void OnLook(InputAction.CallbackContext context)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(context.ReadValue<Vector2>());
+			}
+		}
+
+		public void OnJump(InputAction.CallbackContext context)
+		{
+			switch (context.phase)
+            {
+				case InputActionPhase.Performed:
+					JumpInput(true);
+					break;
+				case InputActionPhase.Canceled:
+					JumpInput(false);
+					break;
+				default: break;
+            }
+		}
+
+		public void OnSprint(InputAction.CallbackContext context)
+		{
+			switch (context.ReadValue<float>())
+			{
+				case 0: // Not being pressed
+					SprintInput(false);
+					break;
+				case 1: // Being pressed
+					SprintInput(true);
+					break;
+				default: break;
+			}
+		}
+#endif
+
+
+		public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			sprint = newSprintState;
+		}
+
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Improved_Movement_Unity_Third_Person_Template
+ Improved the movement and animation controller of the original unity third person template
